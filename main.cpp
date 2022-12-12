96ced0b95eff8d24928c69ce735341cf85894f4c
fabio.vicini <fabio.vicini@polito.it>
2022-12-12 10:37:54 +0100

    fix: solver

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
   std::cerr.precision(16);
   std::cerr<< std::scientific<< "Test "<< test.transpose()<< std::endl;
 
-  Eigen::SparseMatrix<double> A;
+  Eigen::SparseMatrix<double, 0, MKL_INT64> A;
   A.resize(2, 2);
 
   std::list<Eigen::Triplet<double>> triplets;
@@ -30,39 +30,46 @@ int main(int argc, char** argv)
                     triplets.end());
   A.makeCompressed();
 
-  Eigen::VectorXd b;
-  b.resize(2);
-  b[0] = 55.0;
-  b[1] = 123.0;
+  Eigen::VectorXd b = Eigen::VectorXd::Zero(2);
+  b[0] = A.row(0).sum();
+  b[1] = A.row(1).sum();
 
-  Eigen::VectorXd x;
+  Eigen::VectorXd x_Pardiso = Eigen::VectorXd::Zero(2);
 
 #if ENABLE_MKL
-  //Eigen::PardisoLU<Eigen::SparseMatrix<double>> solver(A);
-  Eigen::PardisoLDLT<Eigen::SparseMatrix<double>> solver(A);
-  const Eigen::ComputationInfo solverResult = solver.info();
-  if (solverResult != Eigen::ComputationInfo::Success)
+  //Eigen::PardisoLU<Eigen::SparseMatrix<double, 0, MKL_INT64>> solver_Pardiso;
+  Eigen::PardisoLDLT<Eigen::SparseMatrix<double, 0, MKL_INT64>> solver_Pardiso(A);
+
+  solver_Pardiso.compute(A);
+  const Eigen::ComputationInfo solverResult_Pardiso = solver_Pardiso.info();
+  if (solverResult_Pardiso != Eigen::ComputationInfo::Success)
   {
     throw std::runtime_error("Error " +
-                             std::to_string(static_cast<unsigned int>(solverResult)) +
-                             " in eigen solver");
+                             std::to_string(static_cast<unsigned int>(solverResult_Pardiso)) +
+                             " in eigen Pardiso solver");
   }
 
-  x = solver.solve(b);
-#else
-  Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> solver(A);
-  const Eigen::ComputationInfo solverResult = solver.info();
-  if (solverResult != Eigen::ComputationInfo::Success)
-  {
-    throw std::runtime_error("Error " +
-                             std::to_string(static_cast<unsigned int>(solverResult)) +
-                             " in eigen solver");
-  }
-
-  x = solver.solve(b);
+  x_Pardiso = solver_Pardiso.solve(b);
 #endif
 
-  std::cerr<< std::scientific<< "The solution is: "<< x.transpose()<< std::endl;
+  Eigen::SimplicialLDLT<Eigen::SparseMatrix<double, 0, MKL_INT64>> solver_Eigen(A);
+  const Eigen::ComputationInfo solverResult_Eigen = solver_Eigen.info();
+  if (solverResult_Eigen != Eigen::ComputationInfo::Success)
+  {
+    throw std::runtime_error("Error " +
+                             std::to_string(static_cast<unsigned int>(solverResult_Eigen)) +
+                             " in eigen Eigen solver");
+  }
+
+  Eigen::VectorXd x_Eigen = solver_Eigen.solve(b);
+
+  const Eigen::VectorXd x_Solution = Eigen::VectorXd::Ones(2);
+
+  std::cerr<< std::scientific<< "The Pardiso solution is: "<< x_Pardiso.transpose()<< std::endl;
+  std::cerr<< std::scientific<< "The Pardiso error is: "<< (x_Pardiso - x_Solution).norm()<< std::endl;
+
+  std::cerr<< std::scientific<< "The Eigen solution is: "<< x_Eigen.transpose()<< std::endl;
+  std::cerr<< std::scientific<< "The Eigen error is: "<< (x_Eigen - x_Solution).norm()<< std::endl;
 
   return 0;
 }
